64811c822b3d9d307cb070aaaba5d33d6da46de9
Graham S <graham@sharmandigital.com>
2023-09-22 15:58:02 +0100

    feat: Enhance drawing functionality in grid
    - Transition from CSS hover effect to JS-based drawing.
    - Implemented mouse-based drawing: squares now fill on mouse down and hover.
    - Removed unused  CSS rule for cleaner codebase.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//Event listners to draw on mousedown
+let isDrawing = false;
+
+document.addEventListener('mousedown', () => {
+    isDrawing = true;
+});
+
+document.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+
+//creates grid
 function createGrid(squaresPerSide = 16) {
     const container = document.getElementById("gridContainer");
     const sqaureSize = container.offsetWidth / squaresPerSide;
@@ -11,7 +23,35 @@ function createGrid(squaresPerSide = 16) {
         }
     }
 }
+//function to fill squares on hover
+function addSquareHoverEffect() {
+    document.querySelectorAll("#gridContainer > div").forEach(div => {
+        div.addEventListener('mouseover', function() {
+            if (isDrawing) {
+                div.style.backgroundColor = "black";
+            }
+        });
+    });
+}
+
+//function to reset the grid and select square volume
+function resetGrid() {
+    let sqauresPerSide = prompt("Choose a number of sqaures wide between 1 and 100:");
+    squaresPerSide = parseInt(sqauresPerSide);
+    if (sqauresPerSide <1 || sqauresPerSide > 100 || isNaN(sqauresPerSide)) {
+        alert("Number must be between 1 and 100");
+        return;
+    }
+    document.getElementById("gridContainer").innerHTML = '';
+    createGrid(sqauresPerSide);
+    addSquareHoverEffect();
+}
+
+
+
+document.getElementById("gridSize").addEventListener('click', resetGrid);
 
 window.onload = function() {
     createGrid();
+    addSquareHoverEffect();
 }
